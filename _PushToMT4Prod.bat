7dfdc8355e78981e7e7dfdbdc9d05fdfdb0b9d75
Ofer Kruzel <okruzel@gmail.com>
2021-02-13 16:28:19 +0200

    path update

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_B"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\04_FALCON_B"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\04_FALCON_B"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\04_FALCON_B"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\04_FALCON_B"
+set SOURCE_DIR="C:\Users\Ofer\trading\Lazy Trading course\FALCON_B"
+set DEST_DIR1="C:\Program Files (x86)\MetaTrader 4-1\MQL4\Experts\04_FALCON_B"
+set DEST_DIR2="C:\Program Files (x86)\MetaTrader 4-2\MQL4\Experts\04_FALCON_B"
+set DEST_DIR3="C:\Program Files (x86)\MetaTrader 4-3\MQL4\Experts\04_FALCON_B"
+set DEST_DIR4="C:\Program Files (x86)\MetaTrader 4-4\MQL4\Experts\04_FALCON_B"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
